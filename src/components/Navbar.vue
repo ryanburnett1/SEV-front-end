13d680cc6f7c73d397300677bd6e80e4d53ff080
suptoasty <jason.lonsinger@gmail.com>
2021-02-09 15:05:14 -0600

    Start of Navbar and add code to use Vue-Router

---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container>
+      <div v-if="!$vuetify.breakpoint.mobile">
+        <v-tabs>
+            <v-tab
+                v-for="route in $router.options.routes"
+                :key="route.path"
+                :to="route.path"
+                v-show="showTab(route)"
+            >
+                {{ route.name }}
+            </v-tab>
+        </v-tabs>
+      </div>
+      <div v-else>
+          Burger Menu here
+      </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+    methods: {
+        showTab: function(route) {
+            // list of manual excludes
+            if(route.name in []) return false
+
+            // hide if meta says so in @/router/index.js
+            return !route.meta.hide
+
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,11 +8,19 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+    meta: {
+      hide: false,
+      requiresAuth: false,
+    },
   },
   {
     path: "/about",
     name: "About",
+    meta: {
+      hide: false,
+      requiresAuth: false,
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
