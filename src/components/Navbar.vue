f52ea9fbfbb7ed41f5db7c10f60d5f2c470a4060
suptoasty <jason.lonsinger@gmail.com>
2021-02-09 16:35:39 -0600

    Add Scale Transition to Mobile Menu

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,33 +18,36 @@
       </div>
       <!-- if mobile show option (burger) menu -->
       <div v-else>
-          <v-menu>
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                    v-bind="attrs"
-                    v-on="on"
-                    icon
+        <v-menu 
+            offset-y
+            transition="scroll-y-transition"
+        >
+            <template v-slot:activator="{ on, attrs }">
+            <v-btn
+                v-bind="attrs"
+                v-on="on"
+                icon
+            >
+                <v-icon>mdi-menu</v-icon>
+            </v-btn>
+            </template>
+            <v-list
+            nav
+            >
+                <v-list-item-group>
+                <v-list-item
+                    v-for="route in $router.options.routes"
+                    :key="route.path"
+                    :to="route.path"
                 >
-                    <v-icon>mdi-menu</v-icon>
-                </v-btn>
-              </template>
-              <v-list
-                nav
-              >
-                  <v-list-item-group>
-                    <v-list-item
-                        v-for="route in $router.options.routes"
-                        :key="route.path"
-                        :to="route.path"
-                    >
-                        <v-list-item-icon>
-                            <v-icon v-if="route.meta.icon">{{ route.meta.icon }}</v-icon>
-                        </v-list-item-icon>
-                        <v-list-item-title>{{ route.name }}</v-list-item-title>
-                    </v-list-item>
-                  </v-list-item-group>
-              </v-list>
-          </v-menu>
+                    <v-list-item-icon>
+                        <v-icon v-if="route.meta.icon">{{ route.meta.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-title>{{ route.name }}</v-list-item-title>
+                </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-menu>
       </div>
   </v-container>
 </template>
