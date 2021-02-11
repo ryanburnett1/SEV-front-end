db564fc076060a6c57fbb6ab48d908085df666ad
suptoasty <jason.lonsinger@gmail.com>
2021-02-11 12:40:17 -0600

    Added Reusable ConfirmationDialog

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,46 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <Navbar />
-    </v-app-bar>
+	<v-app>
+		<v-app-bar app color="primary" dark>
+			<template v-slot:img="{ props }">
+				<v-img v-bind="props"> </v-img>
+			</template>
+			<Navbar />
+		</v-app-bar>
 
-    <v-main>
-      <v-container fluid>
-        <router-view></router-view>
-      </v-container>
-    </v-main>
-  </v-app>
+		<v-main>
+			<v-container fluid>
+				<router-view></router-view>
+			</v-container>
+		</v-main>
+	</v-app>
 </template>
 
 <script>
-import Navbar from "@/components/Navbar.vue"
+import Navbar from "@/components/Navbar.vue";
 
 export default {
-  name: "App",
+	name: "App",
 
-  components: {
-    Navbar,
-  },
+	components: {
+		Navbar,
+	},
 
-  data: () => ({
-    //
-  })
+	data: () => ({
+		fab: false,
+	}),
 };
+
+//FAB Menu
+// <v-speed-dial v-model="fab" bottom right absolute>
+// 	<template v-slot:activator>
+// 		<v-skeleton-loader type="button">
+// 			<v-btn v-model="fab" dark fab>
+// 				<v-icon v-if="fab">mdi-close</v-icon>
+// 				<v-icon v-else>mdi-cog</v-icon>
+// 			</v-btn>
+// 		</v-skeleton-loader>
+// 	</template>
+// 	<v-btn>Test</v-btn>
+// 	<v-btn>Other Test</v-btn>
+// </v-speed-dial>
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -5,51 +5,51 @@ import Home from "../views/Home.vue";
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-    meta: {
-      hide: false, // used to hide from navigation menus
-      requiresAuth: false, // if requires user to be logged in
-      icon: "mdi-home", // icon to display in menus
-      role: "", // used to check user roles (admin, member, etc)
-    },
-  },
-  {
-    path: "/about",
-    name: "About",
-    meta: {
-      hide: false,
-      requiresAuth: false,
-      icon: "",
-      role: "",
-    },
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+	{
+		path: "/",
+		name: "Home",
+		component: Home,
+		meta: {
+			hide: false, // used to hide from navigation menus
+			requiresAuth: false, // if requires user to be logged in
+			icon: "mdi-home", // icon to display in menus
+			role: "", // used to check user roles (admin, member, etc)
+		},
+	},
+	{
+		path: "/about",
+		name: "About",
+		meta: {
+			hide: false,
+			requiresAuth: false,
+			icon: "",
+			role: "",
+		},
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () =>
+			import(/* webpackChunkName: "about" */ "../views/About.vue"),
+	},
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  base: process.env.BASE_URL,
-  routes
+	mode: "history",
+	base: process.env.BASE_URL,
+	routes,
 });
 
 router.beforeEach((to, from, next) => {
-  // checks each route and blocks users that are not logged in for routes that require an account
-  let requiresAuth = to.matched.some(function(x) {
-    return x.meta.requiresAuth;
-  });
+	// checks each route and blocks users that are not logged in for routes that require an account
+	let requiresAuth = to.matched.some(function(x) {
+		return x.meta.requiresAuth;
+	});
 
-  if (requiresAuth /* && check user role here */) {
-    next("/login");
-  } else {
-    next();
-  }
+	if (requiresAuth /* && check user role here */) {
+		next("/login");
+	} else {
+		next();
+	}
 });
 
 export default router;
