19b976db354e0596102203e134add100d6f4a048
suptoasty <jason.lonsinger@gmail.com>
2021-02-27 01:36:47 -0600

    Work on redesigning profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,33 +1,108 @@
 <template>
 	<v-container fluid>
 		<v-row>
-			<v-col cols="3">
-				<v-col>
-					<v-img
-						:src="person.picture"
-						:lazy-src="require('@/assets/images/scared-batman.jpg')"
-						max-height="300"
-						max-width="300"
-					></v-img>
-				</v-col>
-				<v-col>
-					<v-subheader>Info</v-subheader>
+			<v-col :cols="$vuetify.breakpoint.mdAndUp ? 3 : 1">
+				<v-navigation-drawer
+					v-model="open"
+					:permanent="$vuetify.breakpoint.mdAndUp"
+					absolute
+				>
+					<v-list dense>
+						<v-list-item dense flat>
+							<v-btn
+								v-if="$vuetify.breakpoint.smAndDown"
+								@click="open = !open"
+								fixed
+								right
+								flat
+							>
+								<v-icon>mdi-close</v-icon>
+							</v-btn>
+						</v-list-item>
+						<v-list-item class="px-2">
+							<v-list-item-avatar>
+								<v-img
+									:src="person.picture"
+									:lazy-src="require('@/assets/images/scared-batman.jpg')"
+								></v-img>
+							</v-list-item-avatar>
+						</v-list-item>
+
+						<v-list-item>
+							<v-list-item-content>
+								<v-list-item-title class="title">
+									{{ person.fullName() }}
+								</v-list-item-title>
+								<!-- <v-list-item-subtitle>{{ user.email }}</v-list-item-subtitle> -->
+							</v-list-item-content>
+						</v-list-item>
+						<v-list-item>
+							<v-list-item-content>
+								<v-expansion-panels accordion>
+									<v-expansion-panel>
+										<v-expansion-panel-header>Info</v-expansion-panel-header>
+										<v-expansion-panel-content>
+											<v-row> Phone: {{ person.maskPhoneNumber() }} </v-row>
+											<v-row>
+												Marital Status: {{ person.marital_status }}</v-row
+											>
+											<v-row> Sex: {{ person.sex }} </v-row>
+										</v-expansion-panel-content>
+									</v-expansion-panel>
+									<v-expansion-panel
+										v-if="!!person.skill && person.skill.length > 0"
+									>
+										<v-expansion-panel-header>Skills</v-expansion-panel-header>
+										<v-expansion-panel-content>
+											<div v-for="skill in person.skill" :key="skill.id">
+												{{ skill.skill_name }}
+											</div>
+										</v-expansion-panel-content>
+									</v-expansion-panel>
+								</v-expansion-panels>
+							</v-list-item-content>
+						</v-list-item>
+					</v-list>
+
 					<v-divider></v-divider>
-					<v-col> Phone: {{ person.maskPhoneNumber() }} </v-col>
-					<v-col> Marital Status: {{ person.marital_status }}</v-col>
-					<v-col> Sex: {{ person.sex }} </v-col>
-				</v-col>
-				<v-col v-if="!!person.skill">
-					<v-col>
-						<v-subheader>Skills</v-subheader>
-						<v-divider></v-divider>
-					</v-col>
-					<v-col v-for="skill in person.skill" :key="skill.id">
-						{{ skill.skill_name }}
-					</v-col>
-				</v-col>
+
+					<v-list nav dense>
+						<v-list-item
+							link
+							@click="
+								$router.push({
+									name: 'MemberEdit',
+									params: { id: 0, isAdd: true },
+								})
+							"
+						>
+							<v-list-item-icon>
+								<v-icon>mdi-account-multiple-plus</v-icon>
+							</v-list-item-icon>
+							<v-list-item-title>Add New Member</v-list-item-title>
+						</v-list-item>
+
+						<v-list-item
+							link
+							@click="
+								$router.push({
+									name: 'MemberEdit',
+									params: { id: 0, isAdd: true },
+								})
+							"
+						>
+							<v-list-item-icon>
+								<v-icon>mdi-hard-hat</v-icon>
+							</v-list-item-icon>
+							<v-list-item-title>Add Skill</v-list-item-title>
+						</v-list-item>
+					</v-list>
+				</v-navigation-drawer>
 			</v-col>
-			<v-col lg="6">
+			<v-col>
+				<v-btn v-if="$vuetify.breakpoint.smAndDown" @click="open = !open"
+					>Open Side Menu</v-btn
+				>
 				<v-col>
 					<h1>{{ person.fullName() }}</h1>
 					<v-subheader>{{ person.title }}</v-subheader>
@@ -94,6 +169,7 @@ export default {
 				f_name: "Default",
 				l_name: "Name",
 			}),
+			open: false,
 		};
 	},
 	mounted() {
@@ -134,5 +210,19 @@ export default {
 <style scoped lang="scss">
 .v-subheader {
 	padding-left: 0px !important;
+}
+
+.v-expansion-panels {
+	.v-expansion-panel {
+		.v-expansion-panel-header {
+			padding-left: 0% !important;
+			padding-right: 0% !important;
+		}
+
+		.v-expansion-panel-content {
+			padding-left: 0% !important;
+			padding-right: 0% !important;
+		}
+	}
 }
 </style>
