933cfcb8d076a4a76f6aef4d6cebdc3a28d8e96d
suptoasty <jason.lonsinger@gmail.com>
2021-03-04 15:23:24 -0600

    Moved background styling to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      dense
-      color="primary"
-      dark
-      elevate-on-scroll
-      elevation="24"
-      hide-on-scroll
-    >
-      <template v-slot:img="{ props }">
-        <v-img v-bind="props"> </v-img>
-      </template>
-      <navbar />
-      <v-spacer></v-spacer>
-      <account-button />
-    </v-app-bar>
+	<v-app
+		id="main"
+		:style="{ background: $vuetify.theme.themes[theme].background }"
+	>
+		<v-app-bar
+			app
+			dense
+			color="primary"
+			dark
+			elevate-on-scroll
+			elevation="24"
+			hide-on-scroll
+		>
+			<template v-slot:img="{ props }">
+				<v-img v-bind="props"> </v-img>
+			</template>
+			<navbar />
+			<v-spacer></v-spacer>
+			<account-button />
+		</v-app-bar>
 
-    <v-main>
-      <v-container fluid>
-        <router-view></router-view>
-      </v-container>
-    </v-main>
+		<v-main>
+			<v-container fluid>
+				<router-view></router-view>
+			</v-container>
+		</v-main>
 
-    <!-- <v-footer app>
+		<!-- <v-footer app>
 			<v-alert></v-alert>
 		</v-footer> -->
-  </v-app>
+	</v-app>
 </template>
 
 <script>
@@ -34,24 +37,30 @@ import Navbar from "@/components/Navbar.vue";
 import AccountButton from "@/components/AccountButton.vue";
 
 export default {
-  name: "App",
+	name: "App",
 
-  components: {
-    Navbar,
-    AccountButton,
-  },
-
-  data: () => ({}),
-  mounted() {
-    this.$store.dispatch("retrieveSkillList");
-    // console.log("Stuff: ", this.$store.getters.getSkillList);
-  },
+	components: {
+		Navbar,
+		AccountButton,
+	},
+	computed: {
+		theme() {
+			return this.$vuetify.theme.dark ? "dark" : "light";
+		},
+	},
+	data: () => ({}),
+	mounted() {
+		this.$store.dispatch("retrieveSkillList");
+		// console.log("Stuff: ", this.$store.getters.getSkillList);
+	},
 };
 </script>
 
 <style lang="scss" scoped>
+.v-app {
+}
 .v-app-bar {
-  // border-bottom-left-radius: 5px !important;
-  // border-bottom-right-radius: 5px !important;
+	// border-bottom-left-radius: 5px !important;
+	// border-bottom-right-radius: 5px !important;
 }
 </style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,131 +1,123 @@
 <template>
-  <v-app
-    id="main"
-    :style="{ background: $vuetify.theme.themes[theme].background }"
-  >
-    <v-container>
-      <v-card>
-        <v-toolbar dark color="primary" dense flat>
-          <v-toolbar-title class="text-body-2 font-weight-bold white--text">
-            Login Form:
-          </v-toolbar-title>
-        </v-toolbar>
-        <validation-observer ref="observer" v-slot="{ invalid, validated }">
-          <v-form class="ma-2 pa-2">
-            <validation-provider
-              name="email"
-              rules="required|email"
-              v-slot="{ errors, valid }"
-            >
-              <v-text-field
-                v-model="user.email"
-                :error-messages="errors"
-                :success="valid"
-                label="E-mail"
-                type="email"
-                prepend-icon="mdi-email"
-                @keydown.enter="$refs.passwordField.focus"
-                autofocus
-              ></v-text-field>
-            </validation-provider>
-            <validation-provider
-              name="password"
-              rules="required"
-              v-slot="{ errors, valid }"
-            >
-              <v-text-field
-                ref="passwordField"
-                v-model="user.password"
-                :error-messages="errors"
-                :success="valid"
-                label="Password"
-                :type="show ? 'text' : 'password'"
-                :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-                prepend-icon="mdi-lock"
-                @click:append="show = !show"
-                @keydown.enter="$refs.loginButton.$el.click"
-              ></v-text-field>
-            </validation-provider>
-            <v-divider class="mb-2"></v-divider>
-            <v-row class="ma-1 pa-1">
-              <v-btn
-                ref="loginButton"
-                :disabled="invalid || !validated"
-                color="success"
-                @click="login()"
-              >
-                Login
-              </v-btn>
-              <v-spacer></v-spacer>
-              <v-btn
-                color="primary"
-                dark
-                @click.stop="
-                  validateReset();
-                  dialog = true;
-                "
-              >
-                Forgot Password
-              </v-btn>
-            </v-row>
-          </v-form>
-        </validation-observer>
-        <validation-observer
-          ref="resetobserver"
-          v-slot="{ invalid, validated }"
-        >
-          <v-dialog v-model="dialog" persistent max-width="600px" eager>
-            <v-card>
-              <v-card-title>
-                <span class="headline">Reset Password</span>
-              </v-card-title>
-              <v-card-text>
-                <v-container>
-                  <v-row>
-                    <v-col cols="12">
-                      <validation-provider
-                        name="email"
-                        rules="required|email"
-                        v-slot="{ errors, valid }"
-                      >
-                        <v-text-field
-                          v-model="resetEmail"
-                          :error-messages="errors"
-                          :success="valid"
-                          label="E-mail"
-                          type="email"
-                          prepend-icon="mdi-email"
-                          @keydown.enter="$refs.resetButton.$el.click"
-                        ></v-text-field>
-                      </validation-provider>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </v-card-text>
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="blue darken-1" text @click="dialog = false">
-                  Cancel
-                </v-btn>
-                <v-btn
-                  ref="resetButton"
-                  color="blue darken-1"
-                  text
-                  @click="
-                    dialog = false;
-                    sendResetRequest();
-                  "
-                  :disabled="invalid || !validated"
-                >
-                  Send
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </validation-observer>
-      </v-card>
-    </v-container>
-  </v-app>
+	<v-container>
+		<v-card>
+			<v-toolbar dark color="primary" dense flat>
+				<v-toolbar-title class="text-body-2 font-weight-bold white--text">
+					Login Form:
+				</v-toolbar-title>
+			</v-toolbar>
+			<validation-observer ref="observer" v-slot="{ invalid, validated }">
+				<v-form class="ma-2 pa-2">
+					<validation-provider
+						name="email"
+						rules="required|email"
+						v-slot="{ errors, valid }"
+					>
+						<v-text-field
+							v-model="user.email"
+							:error-messages="errors"
+							:success="valid"
+							label="E-mail"
+							type="email"
+							prepend-icon="mdi-email"
+							@keydown.enter="$refs.passwordField.focus"
+							autofocus
+						></v-text-field>
+					</validation-provider>
+					<validation-provider
+						name="password"
+						rules="required"
+						v-slot="{ errors, valid }"
+					>
+						<v-text-field
+							ref="passwordField"
+							v-model="user.password"
+							:error-messages="errors"
+							:success="valid"
+							label="Password"
+							:type="show ? 'text' : 'password'"
+							:append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+							prepend-icon="mdi-lock"
+							@click:append="show = !show"
+							@keydown.enter="$refs.loginButton.$el.click"
+						></v-text-field>
+					</validation-provider>
+					<v-divider class="mb-2"></v-divider>
+					<v-row class="ma-1 pa-1">
+						<v-btn
+							ref="loginButton"
+							:disabled="invalid || !validated"
+							color="success"
+							@click="login()"
+						>
+							Login
+						</v-btn>
+						<v-spacer></v-spacer>
+						<v-btn
+							color="primary"
+							dark
+							@click.stop="
+								validateReset();
+								dialog = true;
+							"
+						>
+							Forgot Password
+						</v-btn>
+					</v-row>
+				</v-form>
+			</validation-observer>
+			<validation-observer ref="resetobserver" v-slot="{ invalid, validated }">
+				<v-dialog v-model="dialog" persistent max-width="600px" eager>
+					<v-card>
+						<v-card-title>
+							<span class="headline">Reset Password</span>
+						</v-card-title>
+						<v-card-text>
+							<v-container>
+								<v-row>
+									<v-col cols="12">
+										<validation-provider
+											name="email"
+											rules="required|email"
+											v-slot="{ errors, valid }"
+										>
+											<v-text-field
+												v-model="resetEmail"
+												:error-messages="errors"
+												:success="valid"
+												label="E-mail"
+												type="email"
+												prepend-icon="mdi-email"
+												@keydown.enter="$refs.resetButton.$el.click"
+											></v-text-field>
+										</validation-provider>
+									</v-col>
+								</v-row>
+							</v-container>
+						</v-card-text>
+						<v-card-actions>
+							<v-spacer></v-spacer>
+							<v-btn color="blue darken-1" text @click="dialog = false">
+								Cancel
+							</v-btn>
+							<v-btn
+								ref="resetButton"
+								color="blue darken-1"
+								text
+								@click="
+									dialog = false;
+									sendResetRequest();
+								"
+								:disabled="invalid || !validated"
+							>
+								Send
+							</v-btn>
+						</v-card-actions>
+					</v-card>
+				</v-dialog>
+			</validation-observer>
+		</v-card>
+	</v-container>
 </template>
 
 <script>
@@ -135,56 +127,51 @@ import rest from "@/services/restServices";
 import { ValidationObserver, ValidationProvider } from "vee-validate";
 
 export default {
-  name: "App",
-  computed: {
-    theme() {
-      return this.$vuetify.theme.dark ? "dark" : "light";
-    },
-  },
-  components: {
-    ValidationObserver,
-    ValidationProvider,
-  },
-  data() {
-    return {
-      user: {
-        email: "",
-        password: "",
-      },
-      resetEmail: "",
-      show: false,
-      dialog: false,
-    };
-  },
-  methods: {
-    ...mapActions("account", {
-      login: "login",
-    }),
-    validateReset() {
-      if (this.user.email) {
-        this.resetEmail = Object.assign({}, this.user).email;
-        this.$nextTick(() => {
-          this.$refs.resetobserver.validate();
-        });
-      }
-    },
-    sendResetRequest() {
-      console.log(this.user.email);
-      rest
-        .create(`/user/password-reset-request`, { email: this.resetEmail })
-        .then(res => {
-          console.log(res);
-          alert("thing sent");
-        });
-    },
-    login() {
-      this.$store.dispatch("login", {
-        email: this.user.email,
-        password: this.user.password,
-      });
-    },
-  },
+	components: {
+		ValidationObserver,
+		ValidationProvider,
+	},
+	data() {
+		return {
+			user: {
+				email: "",
+				password: "",
+			},
+			resetEmail: "",
+			show: false,
+			dialog: false,
+		};
+	},
+	methods: {
+		...mapActions("account", {
+			login: "login",
+		}),
+		validateReset() {
+			if (this.user.email) {
+				this.resetEmail = Object.assign({}, this.user).email;
+				this.$nextTick(() => {
+					this.$refs.resetobserver.validate();
+				});
+			}
+		},
+		sendResetRequest() {
+			console.log(this.user.email);
+			rest
+				.create(`/user/password-reset-request`, { email: this.resetEmail })
+				.then(res => {
+					console.log(res);
+					alert("thing sent");
+				});
+		},
+		login() {
+			this.$store.dispatch("login", {
+				email: this.user.email,
+				password: this.user.password,
+			});
+		},
+	},
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+</style>
