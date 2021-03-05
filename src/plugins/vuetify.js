d3006de1bebbcd75da6aeaaf0b02e555fa908883
suptoasty <jason.lonsinger@gmail.com>
2021-03-05 00:16:05 -0600

    work on light theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,23 +3,23 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-import colors from "vuetify/lib/util/colors";
+// import colors from "vuetify/lib/util/colors";
 
 export default new Vuetify({
   theme: {
     themes: {
       light: {
-        primary: colors.indigo.base, // #E53935
-        secondary: colors.indigo.darken2, // #FFCDD2
-        // error: #000
-        // info: #000
-        // success: #000
-        // warning: #000
-        accent: colors.indigo.lighten2, // #3F51B5
-        background: colors.indigo.lighten4,
+        primary: "#1843b2", // #E53935
+        secondary: "#4127a6", // #FFCDD2
+        error: "#FF5252",
+        info: "2196F3",
+        success: "#4CAF50",
+        warning: "#FB8C00",
+        accent: "#6279ae", // #3F51B5
+        // background: ""
+        // background: colors.indigo.lighten4,
       },
       dark: {
-        
         primary: "#1B2D33", // #E53935
         secondary: "#576369",
         error: "#FF5252",
@@ -31,6 +31,6 @@ export default new Vuetify({
       }
     },
     options: { customProperties: true },
-    dark: true,
+    dark: false,
   },
 });
