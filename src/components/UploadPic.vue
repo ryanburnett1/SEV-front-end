69813212c1f5cf50db2026cd35ea312e43c85ce1
ryanburnett1.ryan <ryanburnett1.ryan.burnett@eagles.oc.edu>
2021-03-07 21:50:03 -0600

    Clean up uploadpic component again

--- src/components/UploadPic.vue
+++ src/components/UploadPic.vue
@@ -29,8 +29,6 @@ export default {
 					value.size < 2000000 ||
 					"Avatar size should be less than 2 MB!",
 			],
-			filePath: null,
-			fileName: "",
 		};
 	},
 	methods: {
@@ -43,43 +41,3 @@ export default {
 	},
 };
 </script>
-
-<style scoped>
-/* .file-upload {
-	box-shadow: 2px 2px 9px 2px #ccc;
-	border-radius: 1rem;
-	padding: 2rem;
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	font-size: 1rem;
-	width: 60%;
-	margin: 0 auto;
-}
-
-input {
-	font-size: 0.9rem;
-}
-
-input,
-div,
-button {
-	margin-top: 2rem;
-}
-
-.upload-button {
-	width: 7rem;
-	padding: 0.5rem;
-	background-color: #278be9;
-	color: #fff;
-	font-size: 1rem;
-	font-weight: 500;
-	cursor: pointer;
-	border-radius: 1rem;
-}
-
-.upload-button:disabled {
-	background-color: #b3bcc4;
-	cursor: no-drop;
-} */
-</style>
