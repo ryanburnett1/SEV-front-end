3337dd62fae3d18ec84c9724050259450bf5e390
Nathan Woodruff <nathan.woodruff@eagles.oc.edu>
2021-03-10 15:28:56 -0600

    Remove Batman from 404 page

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,7 +1,8 @@
 <template>
   <v-container>
-    <v-img :src="require('@/assets/images/scared-batman.jpg')"></v-img>
-    <h1>404! Batman is Scared!</h1>
+    <!-- <v-img :src="require('@/assets/images/scared-batman.jpg')"></v-img>
+    <h1>404! Batman is Scared!</h1> -->
+    <h1>Page not Found</h1>
   </v-container>
 </template>
 
