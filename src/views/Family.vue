38b26fa9f1f05c8eb680fd1f723ce5e84e5e6a67
Lane Simpson <lsimpson1040@gmail.com>
2021-04-06 18:30:33 -0500

    update, add, and delete relatinships on save

--- src/views/Family.vue
+++ src/views/Family.vue
@@ -137,15 +137,133 @@ export default {
         params: { id: this.id, isAdd: false },
       });
     },
-    relationshipsEditClose(event) {
+    async relationshipsEditClose(event) {
       //all relationships with no ids are new ones
       if (event.isSave) {
         console.log("isSave");
         console.log(this.minimalRels);
         console.log(event);
-        // event.relationships.forEach(rel => {
 
-        // })
+        if (this.minimalRels.length > 0) {
+          // intersect the event.relationships and relsCopy
+          // put the results in updated, added, and deleted
+          // if there is no match, add it to the added list
+          // if there is a match, add it to the updated list and remove from other list
+          // if there is something leftover add it to the deleted list
+
+          let relsCopy = [];
+          //make a deep copy of minimalRels
+          for (let i = 0; i < this.minimalRels.length; i++) {
+            relsCopy.push(new Relationship(this.minimalRels[i]));
+          }
+
+          let updated = [];
+          let deleted = [];
+          let added = [];
+          event.relationships.forEach(rel => {
+            if (rel.id == undefined) {
+              added.push(rel);
+            } else {
+              //find the matching relationship by id
+              let foundIndex = this.relsCopy.findIndex(r => {
+                return r.id == rel.id;
+              });
+              //if a match was found, check if it has different data
+              if (foundIndex != -1) {
+                let foundRelationship = new Relationship(relsCopy[foundIndex]);
+                if (!foundRelationship.deepEqual(rel)) {
+                  //add that relationship to updated
+                  updated.push(rel);
+                }
+                //remove found relationship from relsCopy
+                if (relsCopy.length == 1) {
+                  relsCopy.clear();
+                } else {
+                  relsCopy.splice(foundIndex, 1);
+                }
+              } else {
+                //if no match was found, add it to the added
+                added.push(rel);
+              }
+            }
+          });
+          //if there are leftover relationships from the relsCopy, then they are deleted
+          if (relsCopy.length > 0) {
+            relsCopy.forEach(r => {
+              deleted.push(r);
+            });
+          }
+          //make the proper requests for updated, added, and deleted
+          //wrap each request in a promise
+          let requests = [];
+          for (let i = 0; i < updated.length; i++) {
+            requests.push(
+              new Promise(resolve => {
+                rest
+                  .update("relationship", updated[i].id, updated[i])
+                  .then(value => {
+                    console.log("then executed");
+                    console.log(value);
+                    resolve(value);
+                  });
+              })
+            );
+          }
+          for (let i = 0; i < added.length; i++) {
+            requests.push(
+              new Promise(resolve => {
+                rest.create("relationship", added[i]).then(value => {
+                  console.log("then executed");
+                  console.log(value);
+                  resolve(value);
+                });
+              })
+            );
+          }
+          for (let i = 0; i < deleted.length; i++) {
+            requests.push(
+              new Promise(resolve => {
+                rest.delete("relationship", deleted[i].id).then(value => {
+                  console.log("then executed");
+                  console.log(value);
+                  resolve(value);
+                });
+              })
+            );
+          }
+          //wait for each promise to complete
+          await Promise.all(requests).then(values => {
+            console.log("all finished");
+            console.log(values);
+          });
+        } else {
+          //add all relationships
+          //wrap each request in a promise
+          let requests = [];
+          let d = new Date();
+          console.log(d.getTime());
+          for (let i = 0; i < event.relationships.length; i++) {
+            requests.push(
+              new Promise(resolve => {
+                rest
+                  .create("relationship", event.relationships[i])
+                  .then(value => {
+                    console.log("then executed");
+                    console.log(value);
+                    resolve(value);
+                  });
+              })
+            );
+          }
+          //wait for each promise to complete
+          await Promise.all(requests).then(values => {
+            console.log("all finished");
+            console.log(values);
+          });
+          console.log(d.getTime());
+        }
+        //wait for the getting of all relationships
+        await this.loadRelationshipPerspective();
       }
       this.relsEditKey = !this.relsEditKey;
     },
