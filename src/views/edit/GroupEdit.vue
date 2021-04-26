bac1dacd69859ce35f14b9562a1dc7bfe400e623
Nathan Woodruff <nathan.woodruff@eagles.oc.edu>
2021-04-26 14:36:54 -0500

    Remove Extra save button

--- src/views/edit/GroupEdit.vue
+++ src/views/edit/GroupEdit.vue
@@ -87,10 +87,6 @@
         <v-btn @click="cancel()" color="error">Cancel</v-btn>
       </v-card-actions>
     </v-card>
-    <v-btn icon @click="save()">
-      <v-icon>content-save-outline</v-icon>
-      Save
-    </v-btn>
     <admin-fab :cancelFunction="cancel" :saveFunction="save"></admin-fab>
   </v-container>
 </template>
